2766d192c682828033ca878c05725bcfa6b31d4d
BernardTheFour <bernardctxy1@gmail.com>
2022-04-25 14:43:38 +0700

    initial upload
    - Java 11
    - Maven
    - Lombok
    - Spring Web
    - JPA Hibernate
    - Postgresql

---
+++ src/test/java/com/cdcone/recipy/RecipyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdcone.recipy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
