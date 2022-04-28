d1efe4dcb50dcc99a569356d5cc37ab804b5b5a7
BernardTheFour <bernardctxy1@gmail.com>
2022-04-28 12:16:10 +0700

    ADD: Basic response so front end know if backend have failed service

--- src/main/java/com/cdcone/recipy/controller/RecipeController.java
+++ src/main/java/com/cdcone/recipy/controller/RecipeController.java
@@ -1,8 +1,10 @@
 package com.cdcone.recipy.controller;
 
 import com.cdcone.recipy.dto.RecipeDtoAdd;
+import com.cdcone.recipy.response.BasicResponse;
 import com.cdcone.recipy.services.RecipeService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,11 +20,12 @@ public class RecipeController {
     private final RecipeService recipeService;
 
     @PostMapping("/add")
-    public void add(@RequestBody RecipeDtoAdd dto){
-        try{
+    public BasicResponse add(@RequestBody RecipeDtoAdd dto) {
+        try {
             recipeService.add(dto);
-        } catch(Exception e){
-            System.out.println("ERROR: " + e);
+        } catch (Exception e) {
+            return new BasicResponse(HttpStatus.BAD_REQUEST, e.toString());
         }
+        return new BasicResponse(HttpStatus.OK, "SUCCESS");
     }
 }
