851d0e653fcd11c6e97e958374e148323b9630fa
BernardTheFour <bernardctxy1@gmail.com>
2022-04-29 15:15:14 +0700

    Unit test: get published recipes

--- src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
+++ src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
@@ -1,15 +1,21 @@
 package com.cdcone.recipy.controller;
 
+import java.util.List;
+
 import com.cdcone.recipy.dto.RecipeDtoAdd;
+import com.cdcone.recipy.dto.RecipeDtoList;
+import com.cdcone.recipy.dto.RecipeSearchDto;
 import com.cdcone.recipy.response.CommonResponse;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 
 @SpringBootTest
@@ -18,35 +24,41 @@ public class RecipeControllerTest {
     @Autowired
     private RecipeController recipeController;
 
-    @Test
-    @Order(1)
-    public void addData() {
-        CommonResponse response = recipeController.add(new RecipeDtoAdd(
+    private static RecipeDtoAdd recipeDtoAdd;
+
+    @BeforeAll
+    public static void init() {
+        recipeDtoAdd = new RecipeDtoAdd(
                 1L,
-                "title2",
+                "title",
                 "overview",
                 "ingredients",
                 "content",
                 "videoURL",
                 true,
-                null));
+                null);
+    }
 
+    @Test
+    @Order(1)
+    public void addData() {
+        CommonResponse response = recipeController.add(recipeDtoAdd);
         Assertions.assertEquals(HttpStatus.OK, response.getStatus());
     }
 
     @Test
     @Order(2)
     public void cantAddData() {
-        CommonResponse response = recipeController.add(new RecipeDtoAdd(
-                1L,
-                "title2",
-                "overview",
-                "ingredients",
-                "content",
-                "videoURL",
-                true,
-                null));
-
+        CommonResponse response = recipeController.add(recipeDtoAdd);
         Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatus());
+    }
+
+    @Test
+    @Order(3)
+    public void getNewlyPublishedRecipes() {
+        Page<RecipeDtoList> result = (Page<RecipeDtoList>) recipeController
+                .getPublishedRecipe(new RecipeSearchDto(10, 0, "", null)).getPayload();
+                
+        Assertions.assertEquals(2, result.getContent().size());
     }
 }
