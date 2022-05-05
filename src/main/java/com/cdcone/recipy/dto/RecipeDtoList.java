498b8b55cd9ddda9591a98b18530d394c5a35f13
arkaanni <arkaannuruliman@gmail.com>
2022-05-05 15:02:26 +0700

    removed image from recipe dto list

--- src/main/java/com/cdcone/recipy/dto/RecipeDtoList.java
+++ src/main/java/com/cdcone/recipy/dto/RecipeDtoList.java
@@ -7,23 +7,18 @@ import lombok.Data;
 @Data
 public class RecipeDtoList {
     private String recipeName;
-    @Lob
-    private Byte[] recipeImage;
+
     private String description;
     private int recipeViews;
     private String author;
-    @Lob
-    private Byte[] authorImage;
     private int authorFollower;
 
 
-    public RecipeDtoList(String recipeName, Byte[] recipeImage, String description, int recipeViews, String author, Byte[] authorImage) {
+    public RecipeDtoList(String recipeName, String description, int recipeViews, String author) {
         this.recipeName = recipeName;
-        this.recipeImage = recipeImage;
         this.description = description;
         this.recipeViews = recipeViews;
         this.author = author;
-        this.authorImage = authorImage;
         authorFollower = -1;
     }
 
