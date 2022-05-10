0056006051a0bd6a7d100b221a02845249b68af7
BernardTheFour <34257362+BernardTheFour@users.noreply.github.com>
2022-05-10 09:16:20 +0700

    Tag bugfix (#21)
    * renaming
    
    - method name
    - URL name
    - method parameter search recipe
    
    * Recipe controller change exception catch
    
    * refractor controller. change dto output
    
    * change error catch
    
    * IRB 89 - Search filter title, author, and tags
    
    * Change edit tag request
    
    * Tag Post fix?

--- src/main/java/com/cdcone/recipy/controller/TagController.java
+++ src/main/java/com/cdcone/recipy/controller/TagController.java
@@ -1,5 +1,6 @@
 package com.cdcone.recipy.controller;
 
+import com.cdcone.recipy.dto.EditTagDto;
 import com.cdcone.recipy.entity.TagEntity;
 import com.cdcone.recipy.response.CommonResponse;
 import com.cdcone.recipy.service.TagService;
@@ -30,20 +31,20 @@ public class TagController {
     @PostMapping
     public ResponseEntity<CommonResponse> addTag(@RequestBody String tagName) {
         try {
-            String savedTag = tagService.saveTag(tagName);
+            TagEntity savedTag = tagService.saveTag(tagName);
             return ResponseEntity.ok(new CommonResponse("success: data saved", savedTag));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new CommonResponse(e.getCause().toString()));
         }
     }
 
-    @PutMapping("{name}")
-    public ResponseEntity<CommonResponse> editTag(@PathVariable String name, @RequestBody String newTag) {
+    @PutMapping("")
+    public ResponseEntity<CommonResponse> editTag(@RequestBody EditTagDto dto) {
         try {
-            String editedTag = tagService.editTag(name, newTag);
-            return ResponseEntity.ok(new CommonResponse("success: data updated", editedTag));
+            tagService.editTag(dto.getTagId(), dto.getTagReplace());
+            return ResponseEntity.ok(new CommonResponse("success: data updated", dto.getTagReplace()));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new CommonResponse(e.getCause().toString()));
+            return ResponseEntity.badRequest().body(new CommonResponse(e.toString()));
         }
     }
 }
--- src/main/java/com/cdcone/recipy/service/TagService.java
+++ src/main/java/com/cdcone/recipy/service/TagService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -19,29 +20,20 @@ public class TagService {
         return tagDao.findAll();
     }
 
-    public String saveTag(String name) {
+    public TagEntity saveTag(String name) {
         TagEntity newTag = new TagEntity(name.toLowerCase());
-        String s;
-        try {
-            tagDao.save(newTag);
-            s = newTag.getName();
-        } catch (Exception e) {
-            s = "Tag already exist";
-        }
-        return s;
+        tagDao.save(newTag);
+        return newTag;
     }
 
-    public TagEntity getById(int id){
+    public TagEntity getById(int id) {
         return tagDao.findById(id).get();
     }
 
-    public String editTag(String old, String tag) {
-        Optional<TagEntity> byName = tagDao.findByName(old);
-        if (byName.isPresent()) {
-            TagEntity tagEntity = byName.get();
-            tagEntity.setName(tag);
-            return tagDao.save(tagEntity).getName();
-        }
-        return "Tag not found";
+    public void editTag(int old, String tag) {
+        Optional<TagEntity> byName = tagDao.findById(old);
+
+        byName.get().setName(tag);
+        tagDao.save(byName.get());
     }
 }
