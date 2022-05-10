56bee6e5e4f59d96223ae3d67a8e529869176bb8
BernardTheFour <bernardctxy1@gmail.com>
2022-05-10 10:50:42 +0700

    Bug fix recipe tags

--- src/main/java/com/cdcone/recipy/repository/RecipeRepository.java
+++ src/main/java/com/cdcone/recipy/repository/RecipeRepository.java
@@ -17,29 +17,31 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
 
-    // this is jpa, not sql
-    @Query("SELECT new com.cdcone.recipy.dto.RecipeDtoList " +
-            "(recipe.title, recipe.overview, recipe.views, user.fullName) " +
-            "FROM RecipeEntity recipe " +
-            "JOIN recipe.user user " +
-            "LEFT JOIN recipe.tags tag " +
-            "WHERE LOWER(recipe.title) LIKE LOWER(CONCAT('%', :titleName, '%')) " +
-            "AND LOWER(user.fullName) LIKE LOWER(CONCAT('%', :authorName, '%')) " +
-            "AND tag.id IN :tagId ")
-    public Page<RecipeDtoList> getPublishedRecipes(
-            @PathParam("titleName") String titleName,
-            @PathParam("authorName") String authorName,
-            @PathParam("tagId") Set<Integer> tagId,
-            Pageable pageable);
+        // this is jpa, not sql
+        @Query("SELECT new com.cdcone.recipy.dto.RecipeDtoList " +
+                        "(recipe.title, recipe.overview, recipe.views, user.fullName) " +
+                        "FROM RecipeEntity recipe " +
+                        "JOIN recipe.user user " +
+                        "LEFT JOIN recipe.tags tag " +
+                        "WHERE recipe.isDraft = FALSE " +
+                        "AND LOWER(recipe.title) LIKE LOWER(CONCAT('%', :titleName, '%')) " +
+                        "AND LOWER(user.fullName) LIKE LOWER(CONCAT('%', :authorName, '%')) " +
+                        "AND tag.id IN :tagId ")
+        public Page<RecipeDtoList> getPublishedRecipes(
+                        @PathParam("titleName") String titleName,
+                        @PathParam("authorName") String authorName,
+                        @PathParam("tagId") Set<Integer> tagId,
+                        Pageable pageable);
 
-    @Query("SELECT new com.cdcone.recipy.dto.RecipeDtoList " +
-            "(recipe.title, recipe.overview, recipe.views, user.fullName) " +
-            "FROM RecipeEntity recipe JOIN recipe.user user " +
-            "ORDER BY recipe.views DESC ")
-    public Set<RecipeDtoList> getPopularRecipes();
+        @Query("SELECT new com.cdcone.recipy.dto.RecipeDtoList " +
+                        "(recipe.title, recipe.overview, recipe.views, user.fullName) " +
+                        "FROM RecipeEntity recipe JOIN recipe.user user " +
+                        "WHERE recipe.isDraft = FALSE " +
+                        "ORDER BY recipe.views DESC ")
+        public Set<RecipeDtoList> getPopularRecipes();
 
-    @Query("SELECT NEW com.cdcone.recipy.dto.RecipeDtoList" +
-            "(r.title, r.overview, r.views, r.user.fullName) " +
-            "FROM RecipeEntity r WHERE r.user.username = :username")
-    Page<RecipeDtoList> findByUsername(@Param("username") String username, Pageable pageable);
+        @Query("SELECT NEW com.cdcone.recipy.dto.RecipeDtoList" +
+                        "(r.title, r.overview, r.views, r.user.fullName) " +
+                        "FROM RecipeEntity r WHERE r.user.username = :username")
+        Page<RecipeDtoList> findByUsername(@Param("username") String username, Pageable pageable);
 }
