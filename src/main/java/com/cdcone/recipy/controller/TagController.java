394a3a11b1f866bb4520a234f6640fa6df9a5f10
arkaanni <arkaannuruliman@gmail.com>
2022-05-12 15:24:42 +0700

    fix bug IRB-199 & IRB 201

--- src/main/java/com/cdcone/recipy/controller/TagController.java
+++ src/main/java/com/cdcone/recipy/controller/TagController.java
@@ -5,6 +5,7 @@ import com.cdcone.recipy.entity.TagEntity;
 import com.cdcone.recipy.response.CommonResponse;
 import com.cdcone.recipy.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +34,13 @@ public class TagController {
     public ResponseEntity<CommonResponse> addTag(@RequestBody String tagName) {
         try {
             TagEntity savedTag = tagService.saveTag(tagName);
-            return ResponseEntity.ok(new CommonResponse("success: data saved", savedTag));
+            String msg = "success: data saved";
+            HttpStatus status = HttpStatus.OK;
+            if (savedTag == null) {
+                msg = "error: tag already exist";
+                status = HttpStatus.BAD_REQUEST;
+            }
+            return ResponseEntity.status(status).body(new CommonResponse(msg, tagName));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new CommonResponse(e.getCause().toString()));
         }
@@ -42,8 +49,12 @@ public class TagController {
     @PutMapping("")
     public ResponseEntity<CommonResponse> editTag(@RequestBody EditTagDto dto) {
         try {
-            tagService.editTag(dto.getTagId(), dto.getTagReplace());
-            return ResponseEntity.ok(new CommonResponse("success: data updated", dto.getTagReplace()));
+            String editTag = tagService.editTag(dto.getTagId(), dto.getTagReplace());
+            HttpStatus status = HttpStatus.OK;
+            if (editTag == null) {
+                status = HttpStatus.BAD_REQUEST;
+            }
+            return ResponseEntity.status(status).body(new CommonResponse(editTag, dto.getTagReplace()));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new CommonResponse(e.toString()));
         }
