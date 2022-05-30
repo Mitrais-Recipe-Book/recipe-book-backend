7e618bd9751d2628385d3beb24b12ed99cd57b2d
arkaanni <arkaannuruliman@gmail.com>
2022-05-30 15:44:37 +0700

    update sign up

---
+++ src/test/java/com/cdcone/recipy/service/AuthServiceTest.java
@@ -0,0 +1,68 @@
+package com.cdcone.recipy.service;
+
+import com.cdcone.recipy.dtoRequest.SignInDto;
+import com.cdcone.recipy.util.CustomUser;
+import com.cdcone.recipy.util.JwtUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class AuthServiceTest {
+    private final AuthenticationManager
+            authenticationManager = mock(AuthenticationManager.class);
+    private final JwtUtil jwtUtil = mock(JwtUtil.class);
+    private AuthService authService;
+
+    @BeforeEach
+    void setUp() {
+        authService = new AuthService(authenticationManager, jwtUtil);
+    }
+
+    @Test
+    void testFailToSignIn() {
+        SignInDto mockSignIn = mock(SignInDto.class);
+        AuthenticationException authenticationException =
+                mock(AuthenticationException.class);
+        when(authenticationManager
+                .authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(authenticationException);
+        Optional<Map<String, Object>> auth = authService.auth(mockSignIn);
+        assertFalse(auth.isPresent());
+    }
+
+    @Test
+    void testSuccessSignIn() {
+        SignInDto mockSignIn = mock(SignInDto.class);
+        Authentication mockAuth = mock(Authentication.class);
+        CustomUser mockUser = mock(CustomUser.class);
+        GrantedAuthority userAuthority = mock(GrantedAuthority.class);
+        when(mockUser.getId()).thenReturn(1L);
+        when(mockUser.getEmail()).thenReturn("mock@mail.com");
+        when(mockUser.getUsername()).thenReturn("mockuser");
+        when(mockUser.getFullName()).thenReturn("mock user");
+        when(mockUser.getAuthorities()).thenReturn(List.of(userAuthority));
+        when(mockSignIn.getUsername()).thenReturn("mockuser");
+        when(mockSignIn.getPassword()).thenReturn("password");
+        when(authenticationManager
+                .authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(mockAuth);
+        when(mockAuth.getPrincipal())
+                .thenReturn(mockUser);
+        when(jwtUtil.signJwt(mockUser)).thenReturn("token_value");
+        Optional<Map<String, Object>> auth = authService.auth(mockSignIn);
+        assertTrue(auth.isPresent());
+        Map<String, Object> authData = auth.get();
+        assertEquals("token_value", authData.get("access_token"));
+    }
+}
