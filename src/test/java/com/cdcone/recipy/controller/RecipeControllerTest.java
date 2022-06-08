c081fca130967d41379be428df8d3ca41e5b797a
BernardTheFour <bernardctxy1@gmail.com>
2022-06-08 10:16:37 +0700

    Write controller unit test

--- src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
+++ src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
@@ -1,76 +1,40 @@
 package com.cdcone.recipy.controller;
 
-import java.util.HashSet;
-import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import com.cdcone.recipy.dtoAccess.RecipeDtoList;
-import com.cdcone.recipy.dtoRequest.RecipeDtoAdd;
-import com.cdcone.recipy.dtoRequest.RecipeSearchDto;
-import com.cdcone.recipy.response.CommonResponse;
-import com.cdcone.recipy.service.RecipeService;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
+import com.cdcone.recipy.dtoRequest.RecipeDtoAdd;
+import com.cdcone.recipy.entity.RecipeEntity;
+import com.cdcone.recipy.service.RecipeService;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RecipeControllerTest {
-    @Autowired
-    private RecipeController recipeController;
+    private static final RecipeService RECIPE_SERVICE = mock(RecipeService.class);
 
-    @Autowired
-    private RecipeService recipeService;
+    private final RecipeDtoAdd RECIPE_DTO_ADD = mock(RecipeDtoAdd.class);
+    private final RecipeEntity RECIPE_ENTITY = mock(RecipeEntity.class);
 
-    private static RecipeDtoAdd recipeDtoAdd;
+    private static RecipeController recipeController;
 
     @BeforeAll
-    public static void init() {
-        Set<Integer> tags = new HashSet<Integer>();
-        tags.add(1);
-        tags.add(2);
-        recipeDtoAdd = new RecipeDtoAdd(
-                1L,
-                tags,
-                "title2",
-                "overview",
-                "ingredients",
-                "content",
-                "videoURL",
-                false);
+    static void init() {
+        recipeController = new RecipeController(RECIPE_SERVICE);
     }
 
     @Test
-    @Order(1)
-    public void addData() {
-        ResponseEntity<CommonResponse> response = recipeController.add(recipeDtoAdd);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
+    void add() {
+        when(RECIPE_SERVICE.add(RECIPE_DTO_ADD))
+                .thenReturn(Pair.of("success", RECIPE_ENTITY));
 
-    @Test
-    @Order(2)
-    public void cantAddData() {
-        ResponseEntity<CommonResponse> response = recipeController.add(recipeDtoAdd);
-        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-    }
-
-    @Test
-    @Order(3)
-    @SuppressWarnings("unchecked")
-    public void getNewlyPublishedRecipes() {
-        Page<RecipeDtoList> result = (Page<RecipeDtoList>) recipeController
-                .getPublishedRecipes(new RecipeSearchDto("", "", new HashSet<>(), 0)).getBody().getPayload();
-
-        Assertions.assertEquals(recipeService.totalRecipes(), result.getContent().size());
+        assertEquals(HttpStatus.OK,
+                recipeController.add(RECIPE_DTO_ADD).getStatusCode());
     }
 }
