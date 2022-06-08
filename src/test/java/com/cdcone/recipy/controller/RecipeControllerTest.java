f2c5474efcedea3385ec94c7bb0a4816e294a4f1
BernardTheFour <bernardctxy1@gmail.com>
2022-06-08 12:46:24 +0700

    Recipe Controller Unit test is done

--- src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
+++ src/test/java/com/cdcone/recipy/controller/RecipeControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
@@ -16,10 +17,12 @@ import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cdcone.recipy.dtoAccess.RecipeDetailDto;
 import com.cdcone.recipy.dtoAccess.RecipeDtoList;
 import com.cdcone.recipy.dtoRequest.RecipeDtoAdd;
 import com.cdcone.recipy.dtoRequest.RecipeSearchDto;
 import com.cdcone.recipy.entity.RecipeEntity;
+import com.cdcone.recipy.entity.UserEntity;
 import com.cdcone.recipy.service.RecipeService;
 import com.cdcone.recipy.util.ImageUtil;
 
@@ -140,5 +143,123 @@ public class RecipeControllerTest {
 
         assertNotEquals(HttpStatus.OK,
                 recipeController.saveRecipePhoto(1L, mockImage).getStatusCode());
+    }
+
+    @Test
+    void addViewer() {
+        when(RECIPE_SERVICE.addViewer(1L))
+                .thenReturn("success");
+
+        assertEquals(HttpStatus.OK,
+                recipeController.addViewer(1L).getStatusCode());
+    }
+
+    @Test
+    void failAddViewer() {
+        when(RECIPE_SERVICE.addViewer(1L))
+                .thenReturn("failed");
+
+        assertNotEquals(HttpStatus.OK,
+                recipeController.addViewer(1L).getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void getPopularRecipes() {
+        Set<RecipeDtoList> mockResult = (Set<RecipeDtoList>) mock(Set.class);
+
+        when(RECIPE_SERVICE.getPopularRecipes(1))
+                .thenReturn(Pair.of("success", mockResult));
+
+        assertEquals(HttpStatus.OK,
+                recipeController.getPopularRecipes(1).getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void failedGetPopularRecipes() {
+        Set<RecipeDtoList> mockResult = (Set<RecipeDtoList>) mock(Set.class);
+
+        when(RECIPE_SERVICE.getPopularRecipes(1))
+                .thenReturn(Pair.of("failed", mockResult));
+
+        assertNotEquals(HttpStatus.OK,
+                recipeController.getPopularRecipes(1).getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void getDiscoverRecipes() {
+        Set<RecipeDtoList> mockResult = (Set<RecipeDtoList>) mock(Set.class);
+
+        when(RECIPE_SERVICE.getDiscoverRecipes(1))
+                .thenReturn(Pair.of("success", mockResult));
+
+        assertEquals(HttpStatus.OK,
+                recipeController.getDiscoverRecipes(1).getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void failedGetDiscoverRecipes() {
+        Set<RecipeDtoList> mockResult = (Set<RecipeDtoList>) mock(Set.class);
+
+        when(RECIPE_SERVICE.getDiscoverRecipes(1))
+                .thenReturn(Pair.of("failed", mockResult));
+
+        assertNotEquals(HttpStatus.OK,
+                recipeController.getDiscoverRecipes(1).getStatusCode());
+    }
+
+    @Test
+    void deleteRecipe() {
+        RecipeDtoList mockResult = mock(RecipeDtoList.class);
+
+        when(RECIPE_SERVICE.deleteRecipe(1L))
+                .thenReturn(Pair.of("success", mockResult));
+
+        assertEquals(HttpStatus.OK,
+                recipeController.deleteRecipe(1L).getStatusCode());
+    }
+
+    @Test
+    void failedDeleteRecipe() {
+        RecipeDtoList mockResult = mock(RecipeDtoList.class);
+
+        when(RECIPE_SERVICE.deleteRecipe(1L))
+                .thenReturn(Pair.of("failed", mockResult));
+
+        assertNotEquals(HttpStatus.OK,
+                recipeController.deleteRecipe(1L).getStatusCode());
+    }
+
+    @Test
+    void getById() {
+        UserEntity mockUser = mock(UserEntity.class);
+
+        when(RECIPE_SERVICE.getById(1L))
+                .thenReturn(Pair.of("succes", RECIPE_ENTITY));
+        when(RECIPE_ENTITY.getUser())
+                .thenReturn(mockUser);
+        when(mockUser.getFullName()).thenReturn("fullname");
+        when(mockUser.getUsername()).thenReturn("username");
+
+        assertEquals(HttpStatus.OK,
+                recipeController.getById(1L).getStatusCode());
+    }
+
+    @Test
+    void failGetById() {
+        UserEntity mockUser = mock(UserEntity.class);
+
+        when(RECIPE_SERVICE.getById(1L))
+                .thenReturn(Pair.of("failed", RECIPE_ENTITY));
+        when(RECIPE_ENTITY.getUser())
+                .thenReturn(mockUser);
+        when(mockUser.getFullName()).thenReturn("fullname");
+        when(mockUser.getUsername()).thenReturn("username");
+
+        assertNotEquals(HttpStatus.OK,
+                recipeController.getById(1L).getStatusCode());
     }
 }
