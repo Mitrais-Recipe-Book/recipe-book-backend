43dabeedd68a4d6c8c360fa562e9ba1dfa21907a
BernardTheFour <bernardctxy1@gmail.com>
2022-06-09 09:41:00 +0700

    Unit test for tag controller done

--- src/test/java/com/cdcone/recipy/controller/TagControllerTest.java
+++ src/test/java/com/cdcone/recipy/controller/TagControllerTest.java
@@ -1,16 +1,19 @@
 package com.cdcone.recipy.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 
+import com.cdcone.recipy.dtoRequest.EditTagDto;
 import com.cdcone.recipy.entity.TagEntity;
 import com.cdcone.recipy.service.TagService;
 
@@ -19,6 +22,7 @@ public class TagControllerTest {
     private static final TagService TAG_SERVICE = mock(TagService.class);
 
     private static final TagEntity TAG_ENTITY = mock(TagEntity.class);
+    private static final EditTagDto EDIT_TAG_DTO = mock(EditTagDto.class);
 
     private static TagController tagController;
     
@@ -35,5 +39,62 @@ public class TagControllerTest {
         when(TAG_SERVICE.getAllTags()).thenReturn(mockResult);
 
         assertEquals(HttpStatus.OK, tagController.getAll().getStatusCode());
+    }
+
+
+    @Test
+    void addTag(){
+        Pair<String, TagEntity> mockResult = Pair.of("success", TAG_ENTITY);
+        
+        when(TAG_SERVICE.saveTag("any")).thenReturn(mockResult);
+
+        assertEquals(HttpStatus.OK, tagController.addTag("any").getStatusCode());
+    }
+    
+    @Test
+    void failedAddTag(){
+        Pair<String, TagEntity> mockResult = Pair.of("failed", TAG_ENTITY);
+        
+        when(TAG_SERVICE.saveTag("any")).thenReturn(mockResult);
+
+        assertNotEquals(HttpStatus.OK, tagController.addTag("any").getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void editTag(){
+        Pair<HttpStatus, Map<String, String>> mockResult = Pair.of(HttpStatus.OK, mock(Map.class));
+
+        when(TAG_SERVICE.editTag(1, "new")).thenReturn(mockResult);
+        when(EDIT_TAG_DTO.getTagId()).thenReturn(1);
+        when(EDIT_TAG_DTO.getTagReplace()).thenReturn("new");
+
+        assertEquals(HttpStatus.OK, tagController.editTag(EDIT_TAG_DTO).getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void failedEditTag(){
+        Pair<HttpStatus, Map<String, String>> mockResult = Pair.of(HttpStatus.BAD_REQUEST, mock(Map.class));
+
+        when(TAG_SERVICE.editTag(1, "new")).thenReturn(mockResult);
+        when(EDIT_TAG_DTO.getTagId()).thenReturn(1);
+        when(EDIT_TAG_DTO.getTagReplace()).thenReturn("new");
+
+        assertNotEquals(HttpStatus.OK, tagController.editTag(EDIT_TAG_DTO).getStatusCode());
+    }
+
+    @Test
+    void deleteTag(){
+        when(TAG_SERVICE.deleteTag(1)).thenReturn(TAG_ENTITY);
+
+        assertEquals(HttpStatus.OK, tagController.deleteTag(1).getStatusCode());
+    }
+
+    @Test
+    void failDeleteTag(){
+        when(TAG_SERVICE.deleteTag(1)).thenReturn(null);
+
+        assertNotEquals(HttpStatus.OK, tagController.deleteTag(1).getStatusCode());
     }
 }
