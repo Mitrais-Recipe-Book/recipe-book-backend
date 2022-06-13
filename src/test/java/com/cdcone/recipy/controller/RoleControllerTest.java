657c8553000d3baf5d149531a7f95f0400572131
BernardTheFour <bernardctxy1@gmail.com>
2022-06-13 09:35:08 +0700

    RoleController unit test done

---
+++ src/test/java/com/cdcone/recipy/controller/RoleControllerTest.java
@@ -0,0 +1,54 @@
+package com.cdcone.recipy.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import com.cdcone.recipy.entity.RoleEntity;
+import com.cdcone.recipy.service.RoleService;
+
+public class RoleControllerTest {
+
+    private static RolesController roleController;
+
+    private static final RoleService roleService = mock(RoleService.class);
+
+    @BeforeAll
+    static void init() {
+        roleController = new RolesController(roleService);
+    }
+
+    @Test
+    void add() {
+        when(roleService.add("any"))
+                .thenReturn("success");
+
+        assertEquals(HttpStatus.OK,
+                roleController.add("any").getStatusCode());
+    }
+
+    @Test
+    void failAdd() {
+        when(roleService.add("any"))
+                .thenReturn("failed");
+
+        assertNotEquals(HttpStatus.OK,
+                roleController.add("any").getStatusCode());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void getAll() {
+        List<RoleEntity> mockResult = mock(List.class);
+        when(roleService.getAll()).thenReturn(mockResult);
+
+        assertEquals(HttpStatus.OK, roleController.getAll().getStatusCode());
+    }
+}
