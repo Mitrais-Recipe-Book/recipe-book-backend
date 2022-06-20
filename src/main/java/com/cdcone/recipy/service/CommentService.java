c49fc3d6b4c8badfe297a13c139ad1d2be96efef
BernardTheFour <bernardctxy1@gmail.com>
2022-06-20 10:08:54 +0700

    Add comment repository

--- src/main/java/com/cdcone/recipy/service/CommentService.java
+++ src/main/java/com/cdcone/recipy/service/CommentService.java
@@ -1,6 +1,10 @@
 package com.cdcone.recipy.service;
 
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
+
+import com.cdcone.recipy.dtoRequest.AddCommentDto;
+import com.cdcone.recipy.entity.CommentEntity;
 
 import lombok.RequiredArgsConstructor;
 
