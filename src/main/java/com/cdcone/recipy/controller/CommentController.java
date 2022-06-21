5dd379dbcc768909e2babf4faa621ada50f2794c
BernardTheFour <bernardctxy1@gmail.com>
2022-06-21 07:17:50 +0700

    Comment controller add @RequestBody annotation

--- src/main/java/com/cdcone/recipy/controller/CommentController.java
+++ src/main/java/com/cdcone/recipy/controller/CommentController.java
@@ -3,13 +3,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdcone.recipy.dtoRequest.AddCommentDto;
 import com.cdcone.recipy.response.CommonResponse;
 import com.cdcone.recipy.service.CommentService;
-
 import lombok.RequiredArgsConstructor;
 
 @CrossOrigin("*")
@@ -21,7 +21,7 @@ public class CommentController {
 
     @PostMapping("{recipeId}/comment/add")
     public ResponseEntity<CommonResponse> addComment(@PathVariable(name = "recipeId") Long recipeId,
-            AddCommentDto dto) {
+            @RequestBody AddCommentDto dto) {
         String result = commentService.add(recipeId, dto);
 
         if (result.charAt(0) == 's') {
