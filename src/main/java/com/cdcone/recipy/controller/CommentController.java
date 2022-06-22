98f733e8d8b825e65c40f596cdbfede395ad0a20
BernardTheFour <bernardctxy1@gmail.com>
2022-06-22 10:05:45 +0700

    get comment page change to request param

--- src/main/java/com/cdcone/recipy/controller/CommentController.java
+++ src/main/java/com/cdcone/recipy/controller/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdcone.recipy.dtoAccess.CommentListDto;
@@ -38,7 +39,7 @@ public class CommentController {
 
     @GetMapping("{recipeId}/comments")
     public ResponseEntity<CommonResponse> getComment(@PathVariable(name = "recipeId") Long recipeId,
-            int page) {
+            @RequestParam int page) {
         Pair<String, Page<CommentListDto>> result = commentService.getComment(recipeId, page);
 
         if (result.getFirst().charAt(0) == 's') {
