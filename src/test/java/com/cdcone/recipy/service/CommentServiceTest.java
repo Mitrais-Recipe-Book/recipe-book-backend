fda92f4847fc56794cb958a9995273383f788cd9
BernardTheFour <bernardctxy1@gmail.com>
2022-06-23 10:11:13 +0700

    Comment Service Test: get comment()

--- src/test/java/com/cdcone/recipy/service/CommentServiceTest.java
+++ src/test/java/com/cdcone/recipy/service/CommentServiceTest.java
@@ -5,11 +5,19 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.util.Pair;
 
+import com.cdcone.recipy.dtoAccess.CommentListDto;
 import com.cdcone.recipy.dtoRequest.AddCommentDto;
 import com.cdcone.recipy.entity.CommentEntity;
 import com.cdcone.recipy.entity.UserEntity;
@@ -17,56 +25,80 @@ import com.cdcone.recipy.repository.CommentRepository;
 
 public class CommentServiceTest {
 
-    private CommentService commentService;
+        private CommentService commentService;
 
-    private final UserService USER_SERVICE = mock(UserService.class);
-    private final RecipeService RECIPE_SERVICE = mock(RecipeService.class);
-    private final CommentRepository COMMENT_REPOSITORY = mock(CommentRepository.class);
+        private final UserService USER_SERVICE = mock(UserService.class);
+        private final RecipeService RECIPE_SERVICE = mock(RecipeService.class);
+        private final CommentRepository COMMENT_REPOSITORY = mock(CommentRepository.class);
 
-    private final UserEntity USER_ENTITY = mock(UserEntity.class);
-    private final CommentEntity COMMENT_ENTITY = mock(CommentEntity.class);
+        private final UserEntity USER_ENTITY = mock(UserEntity.class);
+        private final CommentEntity COMMENT_ENTITY = mock(CommentEntity.class);
 
-    private final AddCommentDto ADD_COMMENT_DTO = mock(AddCommentDto.class);
+        private final AddCommentDto ADD_COMMENT_DTO = mock(AddCommentDto.class);
 
-    @BeforeEach
-    void init() {
-        commentService = new CommentService(USER_SERVICE,
-                RECIPE_SERVICE,
-                COMMENT_REPOSITORY);
-    }
+        private final Pageable PAGEABLE = mock(Pageable.class);
 
-    @Test
-    void add() {
-        Pair<String, UserEntity> mockResult = Pair.of("success", USER_ENTITY);
+        @BeforeEach
+        void init() {
+                commentService = new CommentService(USER_SERVICE,
+                                RECIPE_SERVICE,
+                                COMMENT_REPOSITORY);
+        }
 
-        when(ADD_COMMENT_DTO.getUsername()).thenReturn("user 1");
-        when(ADD_COMMENT_DTO.getComment()).thenReturn("comment");
+        @Test
+        void add() {
+                Pair<String, UserEntity> mockResult = Pair.of("success", USER_ENTITY);
 
-        when(USER_SERVICE.getByUsername(ADD_COMMENT_DTO.getUsername()))
-                .thenReturn(mockResult);
+                when(ADD_COMMENT_DTO.getUsername()).thenReturn("user 1");
+                when(ADD_COMMENT_DTO.getComment()).thenReturn("comment");
 
-        when(COMMENT_REPOSITORY.save(any())).thenReturn(COMMENT_ENTITY);
+                when(USER_SERVICE.getByUsername(ADD_COMMENT_DTO.getUsername()))
+                                .thenReturn(mockResult);
 
-        when(RECIPE_SERVICE.addCommentToRecipe(1L, COMMENT_ENTITY))
-                .thenReturn("success");
+                when(COMMENT_REPOSITORY.save(any())).thenReturn(COMMENT_ENTITY);
 
-        assertEquals('s', commentService.add(1L, ADD_COMMENT_DTO).charAt(0));
-    }
+                when(RECIPE_SERVICE.addCommentToRecipe(1L, COMMENT_ENTITY))
+                                .thenReturn("success");
 
-    @Test
-    void failAddComment(){
-        Pair<String, UserEntity> mockResult = Pair.of("failed", new UserEntity());
+                assertEquals('s', commentService.add(1L, ADD_COMMENT_DTO).charAt(0));
+        }
 
-        when(ADD_COMMENT_DTO.getComment()).thenReturn("comment");
+        @Test
+        void failAddComment() {
+                Pair<String, UserEntity> mockResult = Pair.of("failed", new UserEntity());
 
-        when(USER_SERVICE.getByUsername(ADD_COMMENT_DTO.getUsername()))
-                .thenReturn(mockResult);
+                when(ADD_COMMENT_DTO.getComment()).thenReturn("comment");
 
-        when(COMMENT_REPOSITORY.save(any())).thenReturn(COMMENT_ENTITY);
+                when(USER_SERVICE.getByUsername(ADD_COMMENT_DTO.getUsername()))
+                                .thenReturn(mockResult);
 
-        when(RECIPE_SERVICE.addCommentToRecipe(1L, COMMENT_ENTITY))
-                .thenReturn("success");
+                when(COMMENT_REPOSITORY.save(any())).thenReturn(COMMENT_ENTITY);
 
-        assertEquals('f', commentService.add(1L, ADD_COMMENT_DTO).charAt(0));
-    }
+                when(RECIPE_SERVICE.addCommentToRecipe(1L, COMMENT_ENTITY))
+                                .thenReturn("success");
+
+                assertEquals('f', commentService.add(1L, ADD_COMMENT_DTO).charAt(0));
+        }
+
+        @Test
+        @SuppressWarnings("unchecked")
+        void getComment() {
+                Page<CommentListDto> mockResult = mock(Page.class);
+                when(COMMENT_REPOSITORY.getComments(any(), any()))
+                                .thenReturn(mockResult);
+
+                assertEquals("success: data retrieved",
+                                commentService.getComment(1L, 0).getFirst());
+        }
+
+        @Test
+        @SuppressWarnings("unchecked")
+        void failedGetComment(){
+                Page<CommentListDto> mockResult = mock(Page.class);
+                when(COMMENT_REPOSITORY.getComments(any(), any()))
+                                .thenReturn(mockResult);
+
+                assertEquals("failed: page index must not be less than zero",
+                                commentService.getComment(1L, -1).getFirst());
+        }
 }
